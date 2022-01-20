b27f2a8757674d9305ed86d1ace0e4532910b14b
Allen Lawrence Bolaños <bolanos.allenlawrence@gmail.com>
2022-01-20 16:45:05 +0800

    Created a new TasksList Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
+function TasksList() {
+    return (
+        <ul>
+            <li className="tasks-item">Sleep early</li>
+            <li className="tasks-item">Eat healthy meals</li>
+            <li className="tasks-item">Exercise every once in a while</li>
+        </ul>
+    )
+}
+
 function App() {
     const pStyle = {
         "margin-bottom" : "15px"
@@ -12,11 +22,7 @@ function App() {
             <header className="app-header">
                 <h1>My Amazing ToDo-List App</h1>
                 <p style={pStyle} >The most simple and amazing todo-list React app.</p>
-                <ul>
-                    <li className="tasks-item">Sleep early</li>
-                    <li className="tasks-item">Eat healthy meals</li>
-                    <li className="tasks-item">Exercise every once in a while</li>
-                </ul>
+                <TasksList/>
             </header>
         </div>
     )  
