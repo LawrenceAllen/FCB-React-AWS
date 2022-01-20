7bf31809e4ad6a73383e2fc5b7cbcd71f7ba77ee
Allen Lawrence Bolaños <bolanos.allenlawrence@gmail.com>
2022-01-20 16:30:59 +0800

    Add some CSS styles and implement inline style

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+    const pStyle = {
+        "margin-bottom" : "15px"
+    };
 
     return (
         <div className="app">
             <header className="app-header">
                 <h1>My Amazing ToDo-List App</h1>
-                <p>The most simple and amazing todo-list React app.</p>
+                <p style={pStyle} >The most simple and amazing todo-list React app.</p>
+                <ul>
+                    <li className="tasks-item">Sleep early</li>
+                    <li className="tasks-item">Eat healthy meals</li>
+                    <li className="tasks-item">Exercise every once in a while</li>
+                </ul>
             </header>
         </div>
     )  
