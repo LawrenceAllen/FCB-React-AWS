009a4d8004b4b4e013455f005e59481c31f915ee
Allen Lawrence Bolaños <bolanos.allenlawrence@gmail.com>
2022-01-21 14:07:10 +0800

    Add an input box

--- src/index.js
+++ src/index.js
@@ -9,12 +9,29 @@ function Task(props) {
     )
 }
 
-function TasksList() {
+function TasksList(event) {
+    const taskItemsList = [
+        "Sleep Early",
+        "Eat healthy meals",
+        "Exercise later"
+    ];
 
     return (
-        <ul>
-            <Task taskName="Sleep Early" />
-        </ul>
+        <div>
+            <input className ="task-input" type="text" placeholder="Type your task here"></input>
+            <ul>
+                {taskItemsList.map((task, index) => {
+                    
+
+                    return (
+                    <Task key = {index} taskName={task} />
+                    )
+                }
+                    
+                )}
+            </ul>
+        </div>
+        
     )
 }
 
