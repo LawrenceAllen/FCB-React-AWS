84cee3c4dba0973f31f0f1da8237ea039df7eed7
Allen Lawrence Bolaños <bolanos.allenlawrence@gmail.com>
2022-01-21 15:23:09 +0800

    Update the data in taskItemsList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Task from "./Task";
 
 export default function TasksList(event) {
-    const taskItemsList = [
+    const [taskItemsList, setTaskItemsList] = useState([
         "Sleep Early",
         "Eat healthy meals",
         "Exercise later"
-    ];
+    ]);
+
+    const [taskValue, setTaskValue] = useState();
+
+    const inputChangeHandler = (e) => {
+        setTaskValue(e.target.value)
+    }
+
+    const addTaskHandler = () => {
+        setTaskItemsList([taskValue, ...taskItemsList])
+    }
 
     return (
         <div>
-            <input className ="task-input" type="text" placeholder="Type your task here"></input>
+            <input 
+            className ="task-input" 
+            type="text" 
+            placeholder="Type your task here" 
+            onChange={inputChangeHandler}
+            onBlur={addTaskHandler}
+            value={taskValue}
+            />
+
             <ul>
                 {taskItemsList.map((task, index) => {
                     
